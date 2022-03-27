607b0efe394a8641e451643abe73301494d3ee2a
Sahil Khadka <sahilkhadka767@gmail.com>
2022-03-27 22:55:26 +0545

    overlay added

--- src/components/box-shadows/BoxShadows.jsx
+++ src/components/box-shadows/BoxShadows.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./boxshadows.css"
 import shadows from "../../data";
-import Box from "../box/Box";
 
 const BoxShadows = () => {
+    const [overlay, setOverlay] = useState(false)
+    const DisplayOverlay = () => {
+        return (
+            <>
+                <div className="app__overlay">
+                    Copied!!!👍
+                </div>
+            </>
+        )
+    }
+    const handleClick = (shadow) => {
+        navigator.clipboard.writeText(`box-shadow: ${shadow};`)
+        setOverlay(true)
+        setTimeout(() => {
+            setOverlay(false)
+        }, 2500);
+    }
     return (
         <>
             <h1 className="app__heading">Css Box Shadows</h1>
             <div className="app__wrapper">
-                {shadows.map((item, index) => 
-                    <Box shadow = {item} key = {index} />
+                {shadows.map((item, index) =>
+                    <div
+                        key = {index + 1} 
+                        style={{boxShadow : item}}
+                        className="box__container">
+                        <button
+                            className="glow-on-hover"
+                            onClick={() => handleClick(item)}
+                        >Copy Css</button>
+                    </div>
                 )}
             </div>
+            {overlay &&
+                <DisplayOverlay />
+            }
         </>
     )
 }
